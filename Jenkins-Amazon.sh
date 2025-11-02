f9d9a9cb7845b31f6bbe487fe32d3ec77d98802a
Nandish <nandish1701@gmail.com>
2025-11-02 20:27:25 +0530

    File to setup jenkins in Amazon linux

---
+++ Jenkins-Amazon.sh
@@ -0,0 +1,11 @@
+sudo yum update -y
+sudo amazon-linux-extras enable corretto11
+sudo yum install java-21-amazon-corretto -y
+java -version
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
